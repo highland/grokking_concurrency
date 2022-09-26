186342d2a903ca7541e386f6311d41c7833f7534
Mark Thomas <mark.highland@gmail.com>
2022-09-26 15:34:41 +0100

    Updates to use contexed managed locks

--- Chapter 8/reader_writer/rwlock.py
+++ Chapter 8/reader_writer/rwlock.py
@@ -10,20 +10,18 @@ class RWLock:
         self.write_lock = Lock()
 
     def acquire_read(self):
-        self.read_lock.acquire()
-        self.readers += 1
-        if self.readers == 1:
-            self.write_lock.acquire()
-        self.read_lock.release()
+        with self.read_lock:
+            self.readers += 1
+            if self.readers == 1:
+                self.write_lock.acquire()
 
     def release_read(self):
         assert self.readers >= 1
-        self.read_lock.acquire()
-        self.readers -= 1
-        if self.readers == 0:
-            self.write_lock.release()
-        self.read_lock.release()
-
+        with self.read_lock:
+            self.readers -= 1
+            if self.readers == 0:
+                self.write_lock.release()
+ 
     def acquire_write(self):
         self.write_lock.acquire()
 
--- Chapter 8/reader_writer/rwlock_fair.py
+++ Chapter 8/reader_writer/rwlock_fair.py
@@ -15,26 +15,22 @@ class RWLockFair:
         self.write_lock = Lock()
 
     def acquire_read(self):
-        self.order_lock.acquire()
-        self.read_lock.acquire()
-        self.readers += 1
-        if self.readers == 1:
-            self.write_lock.acquire()
-        self.order_lock.release()
-        self.read_lock.release()
+        with self.order_lock:
+            with self.read_lock:
+                self.readers += 1
+                if self.readers == 1:
+                    self.write_lock.acquire()
 
     def release_read(self):
         assert self.readers >= 1
-        self.read_lock.acquire()
-        self.readers -= 1
-        if self.readers == 0:
-            self.write_lock.release()
-        self.read_lock.release()
+        with self.read_lock:
+            self.readers -= 1
+            if self.readers == 0:
+                self.write_lock.release()
 
     def acquire_write(self):
-        self.order_lock.acquire()
-        self.write_lock.acquire()
-        self.order_lock.release()
+        with self.order_lock:
+            self.write_lock.acquire()
 
     def release_write(self):
         self.write_lock.release()
