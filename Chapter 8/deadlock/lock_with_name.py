1cf037840ad8b3ee21cbeb952028a518cc48bd48
Mark Thomas <mark.highland@gmail.com>
2022-09-27 13:48:55 +0100

    Update lock_with_name.py
    Allow lock to be used with context management.

--- Chapter 8/deadlock/lock_with_name.py
+++ Chapter 8/deadlock/lock_with_name.py
@@ -1,9 +1,20 @@
-"""Don"t mind it - just upgrading a mutex with additional attribute to make examples more explicit"""
+"""Don"t mind it - just upgrading a mutex
+     with additional attribute
+     to make examples more explicit."""
 
 from threading import Lock
 
 
 class LockWithName:
+    """ A standard python lock but with name attribute added. """
     def __init__(self, name: str):
         self.name = name
-        self.lock = Lock()
+        self._lock = Lock()
+
+    def __enter__(self):
+        """ Allows this to be used with context management. """
+        self._lock.acquire()
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        """ Allows this to be used with context management. """
+        self._lock.release()
