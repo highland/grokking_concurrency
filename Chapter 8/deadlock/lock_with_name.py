519d61755960abc3876592899bac31f5536c502a
Mark Thomas <mark.highland@gmail.com>
2022-09-27 14:05:54 +0100

    Update lock_with_name.py
    Avoid having to use composed lock directly, by adding acquire and release methods

--- Chapter 8/deadlock/lock_with_name.py
+++ Chapter 8/deadlock/lock_with_name.py
@@ -7,14 +7,21 @@ from threading import Lock
 
 class LockWithName:
     """ A standard python lock but with name attribute added. """
+
     def __init__(self, name: str):
         self.name = name
         self._lock = Lock()
 
+    def acquire(self):
+        self._lock.acquire()
+
+    def release(self):
+        self._lock.release()
+
     def __enter__(self):
         """ Allows this to be used with context management. """
-        self._lock.acquire()
+        self.acquire()
 
     def __exit__(self, exc_type, exc_value, exc_tb):
         """ Allows this to be used with context management. """
-        self._lock.release()
+        self.release()
