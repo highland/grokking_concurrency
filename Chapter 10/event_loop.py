c0c9fe3fe4fc373bc5b818c4f362590b1a2bb88d
Mark Thomas <mark.highland@gmail.com>
2022-10-01 13:55:25 +0100

    Update event_loop.py
    Keep single deque and encapsulate callback (and next_event) in Event object.
    Add Threading to allow EventLoop to run forevever.

--- Chapter 10/event_loop.py
+++ Chapter 10/event_loop.py
@@ -4,44 +4,58 @@
 
 import time
 from collections import deque
+from threading import Thread
 
 
-class EventLoop:
+class Event:
+    def __init__(self, name: str, callback: callable, next_event = None):
+        self.name = name
+        self.callback = callback
+        self.next_event = next_event
+        
+    def execute(self):
+        self.callback(self)
+        
+    
+class EventLoop(Thread):
     def __init__(self):
+        super().__init__()
         # internal task queue
-        self.events = deque()
-        self.callbacks = {}
+        self.events: deque[Event] = deque()
 
-    def register_event(self, event, callback):
-        self.callbacks[event] = callback
+    def register_event(self, event: Event):
+        self.events.append(event)
 
-    def run_forever(self):
-        # running the loop forever
+    def run(self):
+        self._run_forever()
+
+    def _run_forever(self):
         while True:
             # execute the ready tasks
             while self.events:
                 event = self.events.popleft()
-                callback = self.callbacks[event]
-                callback(event)
+                event.execute()
 
 
-def knock(event):
-    print("Knock, knock.")
+def knock(event: Event):
+    print(event.name)
     time.sleep(1)
     # adding a next task into the event loop task queue
-    event_loop.events.append("who")
+    event_loop.events.append(event.next_event)
 
 
-def who(event):
-    print("Who's there?")
+def who(event: Event):
+    print(event.name)
     time.sleep(2)
 
 
 if __name__ == "__main__":
     event_loop = EventLoop()
-    event_loop.register_event("knock", knock)
-    event_loop.register_event("who", who)
+    replying = Event("Who's there?", who)
+    knocking = Event("Knock-knock", knock, replying)
+    event_loop.register_event(knocking)
+    event_loop.register_event(replying)
     # adding several events
     for _ in range(2):
-        event_loop.events.append("knock")
-    event_loop.run_forever()
+        event_loop.events.append(knocking)
+    event_loop.start()
