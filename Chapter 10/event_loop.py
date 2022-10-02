561c83eee07b01d8643ac8ec5a9c4ac214d56f3e
Mark Thomas <mark.highland@gmail.com>
2022-10-02 12:52:33 +0100

    Update event_loop.py
    Rationalise what is an event, and what is an action triggered by that event

--- Chapter 10/event_loop.py
+++ Chapter 10/event_loop.py
@@ -2,51 +2,55 @@
 
 """Simple single threaded event loop implementation"""
 
-import time
 from collections import deque
 from threading import Thread
+from time import sleep
 
 
 class Event:
-    def __init__(self, name: str, callback: callable, next_event = None):
+    def __init__(self, name: str, action: callable, next_event=None):
         self.name = name
-        self.callback = callback
+        self._action = action
         self.next_event = next_event
-        
-    def execute(self):
-        self.callback(self)
-        
-    
+
+    def execute_action(self):
+        self._action(self)
+
+
 class EventLoop(Thread):
+    """ Maintains a deque of Events and executes them. """
+
     def __init__(self):
         super().__init__()
-        # internal task queue
-        self.events: deque[Event] = deque()
+        # internal event queue
+        self._events: deque[Event] = deque()
 
     def register_event(self, event: Event):
-        self.events.append(event)
+        self._events.append(event)
 
     def run(self):
         self._run_forever()
 
     def _run_forever(self):
         while True:
-            # execute the ready tasks
-            while self.events:
-                event = self.events.popleft()
-                event.execute()
+            # execute the action of the next event
+            while self._events:
+                event = self._events.popleft()
+                event.execute_action()
 
 
 def knock(event: Event):
+    """ A callback which does an action and adds another Event to the deque. """
     print(event.name)
-    time.sleep(1)
-    # adding a next task into the event loop task queue
-    event_loop.events.append(event.next_event)
+    sleep(1)
+    # adding a next event into the event loop event queue
+    event_loop.register_event(event.next_event)
 
 
 def who(event: Event):
+    """ A callback which simply does an action. """
     print(event.name)
-    time.sleep(2)
+    sleep(2)
 
 
 if __name__ == "__main__":
@@ -55,7 +59,7 @@ if __name__ == "__main__":
     knocking = Event("Knock-knock", knock, replying)
     event_loop.register_event(knocking)
     event_loop.register_event(replying)
-    # adding several events
+    # adding several _events
     for _ in range(2):
-        event_loop.events.append(knocking)
+        event_loop.register_event(knocking)
     event_loop.start()
