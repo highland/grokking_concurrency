b5070797226b0c7706d6187940c42a65f2599621
Mark Thomas <mark.highland@gmail.com>
2022-10-06 18:43:17 +0100

    Update stopwatch.py
    Fixed type hints

--- Chapter 6/stopwatch.py
+++ Chapter 6/stopwatch.py
@@ -1,14 +1,16 @@
 import time
 
+seconds = float
+
 
 class Stopwatch:
     """Stopwatch to measure elapsed time"""
-    
+
     def start(self) -> None:
         self.start_time = time.perf_counter()
 
     @property
-    def elapsed_time(self) -> time:
+    def elapsed_time(self) -> float:
         try:
             return time.perf_counter() - self.start_time
         except AttributeError:
