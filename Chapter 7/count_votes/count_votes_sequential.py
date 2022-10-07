3a28cca482c1ce04d4b9dc31b9525df1c2b4839c
Mark Thomas <mark.highland@gmail.com>
2022-10-07 17:27:29 +0100

    Update count_votes_sequential.py
    Improved readability/ understandability with type hints

--- Chapter 7/count_votes/count_votes_sequential.py
+++ Chapter 7/count_votes/count_votes_sequential.py
@@ -2,13 +2,18 @@
 
 """Counting votes sequentially"""
 
-import typing as T
+from typing import List, Mapping
 import random
 from collections import Counter
 
+Candidate = int
+Vote = Candidate
+Count = int
+CountedVotes = Mapping[Candidate, Count]
 
-def process_votes(votes: T.List[int]) -> T.Dict[int, int]:
-    counts = Counter(votes)
+
+def process_votes(votes: List[int]) -> CountedVotes:
+    counts: CountedVotes = Counter(votes)
     return counts
 
 
@@ -17,6 +22,7 @@ if __name__ == "__main__":
     num_voters = 100000
     # generating a huge list of votes
     # each vote is an integer represents the selected candidate
-    votes = [random.randint(1, num_candidates) for _ in range(num_voters)]
+    votes: List[Vote] = [random.randint(
+        1, num_candidates) for _ in range(num_voters)]
     counts = process_votes(votes)
     print(counts)
