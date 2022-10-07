5c2a61d1c18d2214123c1ce4f0b73253f01304a4
Mark Thomas <mark.highland@gmail.com>
2022-10-07 17:37:28 +0100

    Update pipeline.py
    Added type hints

--- Chapter 7/pipeline.py
+++ Chapter 7/pipeline.py
@@ -6,11 +6,13 @@ import time
 from queue import Queue
 from threading import Thread
 
+Washload = str
+
 
 class Washer(Thread):
     """ A thread representing a Washing Machine. """
 
-    def __init__(self, in_queue: Queue, out_queue: Queue):
+    def __init__(self, in_queue: Queue[Washload], out_queue: Queue[Washload]):
         super().__init__()
         self.in_queue = in_queue
         self.out_queue = out_queue
@@ -18,18 +20,18 @@ class Washer(Thread):
     def run(self) -> None:
         while True:
             # get the wash  load from the previous stage.
-            wash_load = self.in_queue.get()
-            print(f"Washing {wash_load}")
+            Washload = self.in_queue.get()
+            print(f"Washing {Washload}")
             time.sleep(1)
             # send the wash load to the next stage
-            self.out_queue.put(f'{wash_load}')
+            self.out_queue.put(f'{Washload}')
             self.in_queue.task_done()
 
 
 class Dryer(Thread):
     """ A thread representing a Dryer. """
 
-    def __init__(self, in_queue: Queue, out_queue: Queue):
+    def __init__(self, in_queue: Queue[Washload], out_queue: Queue[Washload]):
         super().__init__()
         self.in_queue = in_queue
         self.out_queue = out_queue
@@ -37,48 +39,48 @@ class Dryer(Thread):
     def run(self) -> None:
         while True:
             # get the wash load from the previous stage.
-            wash_load = self.in_queue.get()
-            # dry the wash_load
-            print(f"\tDrying {wash_load}")
+            Washload = self.in_queue.get()
+            # dry the Washload
+            print(f"\tDrying {Washload}")
             time.sleep(2)
             # send the wash load to next stage
-            self.out_queue.put(f'{wash_load}')
+            self.out_queue.put(f'{Washload}')
             self.in_queue.task_done()
 
 
 class Folder(Thread):
     """ A thread representing the folding action. """
 
-    def __init__(self, in_queue: Queue):
+    def __init__(self, in_queue: Queue[Washload]):
         super().__init__()
         self.in_queue = in_queue
 
     def run(self) -> None:
         while True:
             # get the wash load from the previous stage.
-            wash_load = self.in_queue.get()
-            # fold the wash_load
-            print(f"\t\tFolding {wash_load}")
+            Washload = self.in_queue.get()
+            # fold the Washload
+            print(f"\t\tFolding {Washload}")
             time.sleep(1)
-            print(f"\t\t\t{wash_load} done")
+            print(f"\t\t\t{Washload} done")
             self.in_queue.task_done()
 
 
 class Pipeline:
     """ Represents a washer, dryer and folder linked by queues. """
 
-    def assemble_laundry_for_washing(self):
-        wash_load_count = 8
-        wash_loads_in = Queue(wash_load_count)
-        for wash_load_num in range(wash_load_count):
-            wash_loads_in.put(f'wash_load no {wash_load_num}')
-        return wash_loads_in
+    def assemble_laundry_for_washing(self) -> Queue[Washload]:
+        Washload_count = 8
+        Washloads_in: Queue[Washload] = Queue(Washload_count)
+        for Washload_num in range(Washload_count):
+            Washloads_in.put(f'Washload no {Washload_num}')
+        return Washloads_in
 
     def run_parallel(self) -> None:
         # set up the queues in the pipeline
         to_be_washed = self.assemble_laundry_for_washing()
-        to_be_dried = Queue()
-        to_be_folded = Queue()
+        to_be_dried: Queue[Washload] = Queue()
+        to_be_folded: Queue[Washload] = Queue()
 
         # start the threads linked by the queues
         Washer(to_be_washed, to_be_dried).start()
