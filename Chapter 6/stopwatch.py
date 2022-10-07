8a6a2ee59f6c8c25d3ea45bbe5a9b74a55dcfe50
Mark Thomas <mark.highland@gmail.com>
2022-10-07 15:15:42 +0100

    Update stopwatch.py
    Added context manager methods

--- Chapter 6/stopwatch.py
+++ Chapter 6/stopwatch.py
@@ -1,6 +1,6 @@
 import time
 
-seconds = float
+Seconds = float
 
 
 class Stopwatch:
@@ -10,9 +10,22 @@ class Stopwatch:
         self.start_time = time.perf_counter()
 
     @property
-    def elapsed_time(self) -> float:
+    def elapsed_time(self) -> Seconds:
         try:
             return time.perf_counter() - self.start_time
         except AttributeError:
             self.start_time = time.perf_counter()
             return 0
+
+    def __enter__(self):
+        print('Stopwatch started')
+        self.start()
+
+    def __exit__(self, type, value, traceback):
+        print(f'Stopwatch stopped at {self.elapsed_time} secs')
+
+
+if __name__ == '__main__':
+    with Stopwatch():
+        print('Doing timed operation')
+        time.sleep(2)
