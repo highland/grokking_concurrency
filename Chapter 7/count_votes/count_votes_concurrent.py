f9bb3f1d6410f4a2d583b885954e8703a84e67be
Mark Thomas <mark.highland@gmail.com>
2022-10-07 17:22:51 +0100

    Update count_votes_concurrent.py

--- Chapter 7/count_votes/count_votes_concurrent.py
+++ Chapter 7/count_votes/count_votes_concurrent.py
@@ -65,5 +65,5 @@ if __name__ == "__main__":
     num_voters = 100000
     # generating a huge list of votes
     # each vote is an integer represents the selected candidate
-    votes = [random.randint(1, num_candidates) for _ in range(num_voters)]
+    votes: List[Vote] = [random.randint(1, num_candidates) for _ in range(num_voters)]
     process_votes(votes)
