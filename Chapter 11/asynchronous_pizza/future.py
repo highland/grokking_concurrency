74291825e0bf6f78278781dd6bd3770cef0231c6
Mark Thomas <mark.highland@gmail.com>
2022-11-18 16:35:45 +0000

    Update future.py
    Added type hints

--- Chapter 11/asynchronous_pizza/future.py
+++ Chapter 11/asynchronous_pizza/future.py
@@ -1,23 +1,25 @@
 """The best known way to see the future is to wait"""
 
+from typing import Coroutine, Generator, Awaitable, Any
+from event_loop import EventLoop
 
-class Future:
-    def __init__(self, loop) -> None:
+
+class Future(Awaitable[Any]):
+    def __init__(self, loop: EventLoop) -> None:
         self.loop = loop
         self.done = False
 
-    def set_coroutine(self, co):
+    def set_coroutine(self, co: Coroutine[Any, Any, Any]) -> None:
         self.co = co
 
-    def set_result(self, result):
+    def set_result(self, result: Any) -> None:
         self.done = True
         self.result = result
 
         if self.co:
             self.loop.add_coroutine(self.co)
 
-    def __await__(self):  # This 'Magic Method' is what makes Future a future
+    def __await__(self) -> Generator[Any, None, Any]:  # This 'Magic Method' is what makes Future a future
         if not self.done:
             yield self
         return self.result
-    
