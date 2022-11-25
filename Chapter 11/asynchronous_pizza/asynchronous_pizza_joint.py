c77da9efbfb7d01efc49a7c84d86b27024f5c99c
Mark Thomas <mark.highland@gmail.com>
2022-11-25 16:56:50 +0000

    Update asynchronous_pizza_joint.py
    Remove redundant event loop (ThreadPoolExecutor does it all)

--- Chapter 11/asynchronous_pizza/asynchronous_pizza_joint.py
+++ Chapter 11/asynchronous_pizza/asynchronous_pizza_joint.py
@@ -1,84 +1,61 @@
-import socket
-import time
+#!/usr/bin/env python3
 
+"""Multithreaded echo server implementation"""
+
+from socket import socket, create_server
 from concurrent.futures import ThreadPoolExecutor
-from async_socket import AsyncSocket
-from event_loop import EventLoop
 
+# the maximum amount of data to be received at once
 BUFFER_SIZE = 1024
-HOST = "127.0.0.1"  # address of the host machine
-PORT = 12345  # port to listen on (non-privileged ports are > 1023)
+ADDRESS = ("127.0.0.1", 12345)   # address and port of the host machine
 
 
-class Kitchen:
-    @staticmethod
-    def cook_pizza(n):
-        print(f"Started cooking {n} pizzas")
-        time.sleep(n)
-        print(f"Fresh {n} pizzas are ready!")
+def handle_request(conn: socket) -> None:
+    print(f"Connected to {conn.getpeername()}")
+    try:
+        while (data := conn.recv(BUFFER_SIZE)):
+            try:
+                order = int(data.decode())
+                response = f"Thank you for ordering {order} pizzas\n"
+            except ValueError:
+                response = f"Unrecognisable order, '{data!r}' - please try again\n"
+            print(f"Sending message to {conn.getpeername()}")
+            # send a response
+            conn.send(response.encode())
+    finally:
+        # server expects the client to close its side of the connection when it’s done.
+        # In a real application, we should use timeout for clients if they don’t send
+        # a request after a certain amount of time.
+        print(f"Connection with {conn.getpeername()} has been closed")
+        conn.close()
 
 
 class Server:
-    def __init__(self, loop):
-        self.executor = ThreadPoolExecutor() # to run Tasks in separte threads
-        self.loop = loop
-        # AF_UNIX and SOCK_STREAM are constants represent the protocol and socket type respectively
-        # here we create a TCP/IP socket
-        self.server_socket = AsyncSocket(
-            sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM), loop=loop)
-        # allows multiple sockets to be bound to an identical socket address
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
+    def __init__(self) -> None:
+        self.pool = ThreadPoolExecutor()
         try:
-            print(f"Starting up on: {HOST}:{PORT}")
-            # bind a socket to a specific network interface and port number
-            self.server_socket.bind((HOST, PORT))
-            print("Listen for incoming connections")
-            # on server side let"s start listening mode for this socket
+            print(f"Starting up at: {ADDRESS}")
+            self.server_socket = create_server(ADDRESS)
+            print("Listening for incoming connections")
+            # on server side let's start listening mode for this socket
+            self.server_socket.settimeout(60)  # Don't wait forever
             self.server_socket.listen()
             print("Waiting for a connection")
-            self.server_socket.setblocking(False)
-        except Exception:
+        except OSError:
             self.server_socket.close()
             print("\nServer stopped.")
-        self.loop.add_coroutine(self.serve_forever())
 
-    async def serve_forever(self):
+    def start(self) -> None:
         try:
             while True:
-                conn, address = await self.server_socket.accept()
-                print(f"Connected to {address}")
-                self.loop.add_coroutine(self.serve(conn))
+                conn, address = self.server_socket.accept()
+                print(f"Client connection request from {address}")
+                self.pool.submit(handle_request, conn)
         finally:
-            self.server_socket.close()
-            print("\nServer stopped.")
-
-    async def serve(self, conn):
-        try:
-            while True:
-                data = await conn.recv(BUFFER_SIZE)
-                if not data:
-                    break
-                try:
-                    order = int(data.decode())
-                    response = f"Thank you for ordering {order} pizzas\n"
-                    print(f"Sending message to {conn.getpeername()}")
-                    await conn.send(response.encode())
-                    self.executor.submit(Kitchen.cook_pizza, order)
-                    print(f"Sending message to {conn.getpeername()}")
-                    await conn.send(f"You order on {order} pizzas is ready!\n".encode())
-                except ValueError:
-                    response = "Wrong number of orders, please try again\n"
-                    print(f"Sending message to {conn.getpeername()}")
-                    await conn.send(response.encode())
-            print(f"Connection with {conn.getpeername()} has been closed")
-            conn.close()
-        except Exception:
             self.server_socket.close()
             print("\nServer stopped.")
 
 
 if __name__ == "__main__":
-    loop = EventLoop()
-    server = Server(loop=loop)
-    loop.run_forever()
+    server = Server()
+    server.start()
