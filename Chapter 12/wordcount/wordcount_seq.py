93cd47e90e597c69a34d40f60b7631d0def48fc7
Mark Thomas <mark.highland@gmail.com>
2022-12-01 17:49:21 +0000

    Create wordcount_seq.py
    Updated version

---
+++ Chapter 12/wordcount/wordcount_seq.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+
+from typing import List, Dict
+
+Pathname = str
+Word = str
+
+
+def wordcount(filenames: List[Pathname]) -> Dict[Word, int]:
+    word_counts: Dict[Word, int] = {}
+    for filename in filenames:
+        print(f"Calculating {filename}")
+        with open(filename) as f:
+            for line in f:
+                words = line.lower().split()
+                for word in words:
+                    word = word.lower()
+                    word_counts[word] = 1 + word_counts.get(word, 0)
+    return word_counts
+
+
+if __name__ == "__main__":
+    data = list(
+        glob.glob(f"{os.path.abspath(os.getcwd())}/input_files/*.txt"))
+    result = wordcount(data)
+    print(result)
+
+"""
+Alternative to the above using Python Standard Library:
+    
+    from collections import Counter
+    
+    def wordcount(filenames: List[Pathname]) -> Dict[Word, int]:
+        word_counts: Counter[Word] = Counter()
+        for filename in filenames:
+            print(f"Calculating {filename}")
+            with open(filename) as f:
+                for line in f:
+                    words = line.lower().split()
+                    word_counts.update(words)
+        return word_counts
+"""
